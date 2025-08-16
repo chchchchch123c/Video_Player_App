df0a789a306b7178cec4caa38902ee4bf8df2d33
LEGION <chchchchch123c@gmail.com>
2025-08-16 20:24:56 +0900

    2025-08-16

--- lib/widgets/player_controls/components/seek_slider.dart
+++ lib/widgets/player_controls/components/seek_slider.dart
@@ -5,20 +5,34 @@ import 'package:video_player/video_player.dart';
 
 class SeekSlider extends StatelessWidget {
   final VideoPlayerController controller;
+  final bool isShow;
 
   /// 영상의 현재 위치를 보여주고 조절할 수 있는 슬라이더 위젯.
   /// 슬라이더를 움직이면 해당 위치로 영상이 이동함.
-  const SeekSlider({super.key, required this.controller});
+  const SeekSlider({super.key, required this.controller, required this.isShow});
 
   @override
   Widget build(BuildContext context) {
-    return Slider(
-      thumbColor: kIndigoAccent,
-      activeColor: kIndigoAccent,
-      max: controller.value.duration.inSeconds.toDouble(),
-      value: controller.value.position.inSeconds.toDouble(),
-      onChanged: (value) {
-        controller.seekTo(Duration(seconds: value.toInt()));
-      },);
+    return IgnorePointer(
+      ignoring: !isShow,
+      child: AnimatedOpacity(
+        duration: const Duration(milliseconds: 300),
+        opacity: isShow ? 1.0 : 0.0,
+        child: SliderTheme(
+          data: SliderThemeData(
+            thumbShape: HandleThumbShape(),
+            thumbSize: WidgetStateProperty.all(Size(15, 15))
+          ),
+          child: Slider(
+            thumbColor: kIndigoAccent,
+            activeColor: kIndigoAccent,
+            max: controller.value.duration.inSeconds.toDouble(),
+            value: controller.value.position.inSeconds.toDouble(),
+            onChanged: (value) {
+              controller.seekTo(Duration(seconds: value.toInt()));
+            },),
+        ),
+      ),
+    );
   }
 }
