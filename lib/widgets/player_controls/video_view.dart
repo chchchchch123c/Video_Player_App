a22fa2e53ae7c073df0aef3796c99d30598932d0
LEGION <chchchchch123c@gmail.com>
2025-08-16 20:25:02 +0900

    2025-08-16

--- lib/widgets/player_controls/video_view.dart
+++ lib/widgets/player_controls/video_view.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:video_play_app/widgets/player_controls/components/dimmed_overlay.dart';
+import 'package:video_play_app/widgets/player_controls/components/orientation_button.dart';
 import 'package:video_play_app/widgets/player_controls/components/play_button.dart';
-import 'package:video_play_app/widgets/player_controls/components/ripple_zone.dart';
+import 'package:video_play_app/widgets/ripple/ripple_zone.dart';
 import 'package:video_play_app/widgets/player_controls/components/seek_slider.dart';
+import 'package:video_play_app/widgets/player_controls/components/time_indicator.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoView extends StatelessWidget {
   final VideoPlayerController controller;
   final bool isVideoEnded;
   final VoidCallback onEndReset;
+  final bool isShow;
+  final VoidCallback onShowTap;
 
   /// 영상 플레이어 전체 UI를 구성하는 위젯.
   /// 실제 영상 출력, 제스처 영역, 슬라이더, 재생 버튼 등을 포함함.
@@ -16,39 +21,46 @@ class VideoView extends StatelessWidget {
     required this.controller,
     required this.isVideoEnded,
     required this.onEndReset,
+    required this.isShow,
+    required this.onShowTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: controller.value.aspectRatio,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          VideoPlayer(controller),
-          Positioned.fill(
-            child: Material(
-              color: Colors.transparent,
-              child: RippleZone(controller: controller),
+    return GestureDetector(
+      onTap: onShowTap,
+      child: AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            VideoPlayer(controller),
+            DimmedOverlay(isShow: isShow),
+            RippleZone(controller: controller),
+            Positioned(
+              bottom: 32,
+              left: 18,
+              child: TimeIndicator(controller: controller, isShow: isShow),
             ),
-          ),
-          Positioned(
-            left: -6,
-            right: 12,
-            bottom: 0,
-            child: SeekSlider(controller: controller),
-          ),
-          Positioned(
-            bottom: 0,
-            right: -6,
-            child: IconButton(onPressed: () {}, icon: Icon(Icons.fullscreen)),
-          ),
-          PlayButton(
-            controller: controller,
-            isVideoEnded: isVideoEnded,
-            onReplayPressed: onEndReset,
-          ),
-        ],
+            Positioned(
+              left: -6,
+              right: 12,
+              bottom: 0,
+              child: SeekSlider(controller: controller, isShow: isShow),
+            ),
+            Positioned(
+              bottom: 0,
+              right: -6,
+              child: OrientationButton(isShow: isShow),
+            ),
+            PlayButton(
+              controller: controller,
+              isVideoEnded: isVideoEnded,
+              onReplayPressed: onEndReset,
+              isShow: isShow,
+            ),
+          ],
+        ),
       ),
     );
   }
