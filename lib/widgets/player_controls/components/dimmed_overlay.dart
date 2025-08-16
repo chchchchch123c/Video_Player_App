834e43eaaaf86c0bfb1d9c8af85c661fdf98153e
LEGION <chchchchch123c@gmail.com>
2025-08-16 20:24:32 +0900

    2025-08-16

---
+++ lib/widgets/player_controls/components/dimmed_overlay.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:video_play_app/constant/color.dart';
+
+class DimmedOverlay extends StatelessWidget {
+  final bool isShow;
+
+  /// 영상 위에 흐려진 반투명 오버레이를 보여주는 위젯.
+  /// UI 컨트롤러들이 보일 때 뒷배경을 어둡게 처리함.
+  const DimmedOverlay({super.key, required this.isShow});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      duration: const Duration(milliseconds: 300),
+      opacity: isShow ? 1.0 : 0.0,
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: kBlack.withOpacity(0.5),
+      ),
+    );
+  }
+}
