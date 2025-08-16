969fde462084283f5c7dd3025275c1c358d1ee1f
LEGION <chchchchch123c@gmail.com>
2025-08-16 20:24:35 +0900

    2025-08-16

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_play_app/constant/color.dart';
 import 'package:video_play_app/widgets/player_controls/video_view.dart';
 import 'package:video_player/video_player.dart';
 
@@ -12,65 +13,79 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late VideoPlayerController videoPlayerController;
   bool isVideoEnded = false;
+  bool isShow = true;
 
   @override
   void initState() {
     super.initState();
     videoPlayerController = VideoPlayerController.asset(
-      'assets/videos/(720p)Never_Gonna_Give_You_Up.mp4'
-    )
-    // videoPlayerController = VideoPlayerController.networkUrl(
-    //     Uri.parse(rickRoll))
-      ..initialize().then((_) {
+      'assets/videos/(720p)Old_Town_Road.mp4'
+    )..initialize().then((_) {
         setState(() {});
       },);
 
     videoPlayerController.addListener(() {
-      if (videoPlayerController.value.position >= videoPlayerController.value.duration && !videoPlayerController.value.isPlaying && mounted) {
-        setState(() {
-          isVideoEnded = true;
-        });
-      }
+      if (!mounted) return;
+
+      final controller = videoPlayerController;
+      final isEnded = controller.value.position >= controller.value.duration;
+
+      setState(() {
+        isVideoEnded = isEnded;
+      });
     },);
-
-    videoPlayerController.addListener(() {
-      setState(() {});
-    },);
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: videoPlayerController.value.isInitialized ?
-            VideoView(
-              controller: videoPlayerController,
-              isVideoEnded: isVideoEnded,
-              onEndReset: () {
-                videoPlayerController.seekTo(Duration.zero);
-                videoPlayerController.play();
-                setState(() {
-                  isVideoEnded = false;
-                });
-              },
-            )
-         : AspectRatio(
-          aspectRatio: 16 / 9,
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(4))
-            ),
-          ),
-        ),
-      ),
-    );
   }
 
   @override
   void dispose() {
     videoPlayerController.dispose();
     super.dispose();
+  }
+
+  void onEndReset() {
+    setState(() {
+      isVideoEnded = false;
+    });
+    videoPlayerController.seekTo(Duration.zero);
+    videoPlayerController.play();
+  }
+
+  void onShowTap() {
+    setState(() {
+      isShow = !isShow;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBlack,
+      body: Center(
+        child: videoPlayerController.value.isInitialized ?
+         _body() : _placeHolder(),
+      ),
+    );
+  }
+
+  Widget _body() {
+    return VideoView(
+      controller: videoPlayerController,
+      isVideoEnded: isVideoEnded,
+      onEndReset: onEndReset,
+      onShowTap: onShowTap,
+      isShow: isShow,
+      );
+  }
+
+  Widget _placeHolder() {
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+        ),
+      ),
+    );
   }
 }
