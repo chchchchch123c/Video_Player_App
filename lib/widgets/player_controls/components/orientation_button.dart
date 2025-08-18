bb9fbf3d31a4524639ecd89a01fe733a70a0a8b4
LEGION <chchchchch123c@gmail.com>
2025-08-18 19:33:19 +0900

    2025-08-18

--- lib/widgets/player_controls/components/orientation_button.dart
+++ lib/widgets/player_controls/components/orientation_button.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 class OrientationButton extends StatelessWidget {
   final bool isShow;
+  final VoidCallback onFullScreenToggle;
 
   /// 전체화면 전환 버튼 위젯.
-  /// [isShow]가 true일 때만 보이며, 터치 가능함.
-  const OrientationButton({super.key, required this.isShow});
+  /// [isShow]가 true일 때만 보이며, 눌렀을 때 전체화면 전환 로직을 실행.
+  const OrientationButton({
+    super.key,
+    required this.isShow,
+    required this.onFullScreenToggle,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +20,7 @@ class OrientationButton extends StatelessWidget {
         duration: const Duration(milliseconds: 300),
         opacity: isShow ? 1.0 : 0.0,
         child: IconButton(
-          onPressed: () {},
+          onPressed: onFullScreenToggle,
           icon: Icon(Icons.fullscreen),
         ),
       ),
