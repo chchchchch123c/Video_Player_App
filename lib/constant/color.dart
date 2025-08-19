bb783d613ce93e27cb8764c4756424386e067412
LEGION <chchchchch123c@gmail.com>
2025-08-19 19:51:46 +0900

    2025-08-19

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -2,4 +2,7 @@ import 'dart:ui';
 
 const Color kWhite = Color(0xFFFFFFFF);
 const Color kBlack = Color(0xFF000000);
+const Color kHalfBlack = Color(0x80000000);
+const Color kEightyBlack = Color(0xCC000000);
+const Color kTwentyBlack = Color(0x33000000);
 const Color kIndigoAccent = Color(0xFF536DFE);
