c15511b43743935bcc0cb215341b034c7a504913
LEGION <chchchchch123c@gmail.com>
2025-08-22 23:54:38 +0900

    2025-08-22

---
+++ lib/presentations/screens/select_video_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:video_play_app/constant/color.dart';
+import 'package:video_play_app/presentations/controllers/select_video_controller.dart';
+import 'package:video_play_app/widgets/select_controls/select_container.dart';
+
+class SelectVideoScreen extends StatelessWidget {
+  final SelectVideoController controller;
+
+  const SelectVideoScreen({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kGray,
+      appBar: _AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 12),
+              child: SelectContainer(
+                height: 56.0,
+                icon: Icons.help_outline,
+                text: 'How to use',
+                onTap: () => showDialog(context: context, builder: (context) => AlertDialog(),),
+                // TODO
+              ),
+            ),
+            Expanded(
+              child: SelectContainer(
+                icon: Icons.add_photo_alternate_outlined,
+                text: 'Load a video',
+                onTap: () {
+                  controller.pickVideo(context);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget implements PreferredSizeWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: kIndigoAccent,
+      title: Text('VideoPlayerApp'),
+      titleTextStyle: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+        color: kWhite,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
