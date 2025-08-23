caa9b074b0b0af9fa1ccdfa1450c2a07ed6c0c7d
LEGION <chchchchch123c@gmail.com>
2025-08-24 01:24:24 +0900

    2025-08-24

--- lib/presentations/controllers/select_video_controller.dart
+++ lib/presentations/controllers/select_video_controller.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:video_play_app/constant/url.dart';
 import 'package:video_play_app/presentations/controllers/home_screen_controller.dart';
 import 'package:video_play_app/presentations/screens/home_screen.dart';
 
 class SelectVideoController {
+  final Uri _url = Uri.parse(githubUrl);
 
   Future<void> pickVideo(BuildContext context) async {
     XFile? pickedVideo = await ImagePicker().pickVideo(source: ImageSource.gallery);
@@ -13,6 +16,12 @@ class SelectVideoController {
         pickedVideo: pickedVideo,
       );
     },));
+  }
+
+  Future<void> urlLauncher() async {
+    if (await canLaunchUrl(_url) && !await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
   }
 
 }
